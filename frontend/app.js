70de0c05f1ae1e78fcd7a5dc5f4e19b11e1e4e5e
Vihaan Philip <viha0001@student.monash.edu>
2023-07-31 20:30:50 +0800

    create dummy login button

---
+++ frontend/app.js
@@ -0,0 +1,8 @@
+const inputUsername = document.getElementById('input_username');
+const inputPassword = document.getElementById('input_password');
+
+function attempt_login() {
+    console.log('Attempting login...');
+    console.log('Username: ' + inputUsername.value);
+    console.log('Password: ' + inputPassword.value);
+}
