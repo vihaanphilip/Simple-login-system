e5511bfcb7656d2589ef74fae227388ff0b99105
Vihaan Philip <viha0001@student.monash.edu>
2023-08-01 01:38:57 +0800

    fix login authentication

--- frontend/app.js
+++ frontend/app.js
@@ -1,11 +1,31 @@
-const inputUsername = document.getElementById('input_username');
-const inputPassword = document.getElementById('input_password');
+const baseUrl = "http://localhost:8000";
 
-function attempt_login() {
-    console.log('Attempting login...');
-    console.log('Username: ' + inputUsername.value);
-    console.log('Password: ' + inputPassword.value);
-    fetch('http://localhost:8000/users')
-        .then(response => response.json())
-        .then(data => console.log(data));
-}
+document.addEventListener("DOMContentLoaded", function () {
+
+    const loginButton = this.getElementById('login_button');
+    loginButton.addEventListener('click', function() {
+        const inputUsername = document.getElementById('input_username').value;
+        const inputPassword = document.getElementById('input_password').value;
+
+        console.log(inputUsername);
+        console.log(inputPassword);
+
+        fetch(baseUrl + '/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username: inputUsername,
+                password: inputPassword
+            })
+        })
+        .then(response => {
+            if (response.ok) {
+                console.log('Login successful!');
+            } else {
+                console.log('Login failed!');
+            }
+        })
+    });
+});
